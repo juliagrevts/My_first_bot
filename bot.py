351e9e2d55a730f0827b1dfe24321bc38e47c625
Julia Grevtseva <j.grevts@gmail.com>
2021-08-25 23:52:57 +0300

    Добавила команду planet, планету достала из списка

--- bot.py
+++ bot.py
@@ -1,5 +1,7 @@
 import logging
+
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from ephem import *
 
 import settings
 
@@ -12,6 +14,27 @@ def greet_user(update, context):
     print('Вызван /start')
     update.message.reply_text('Привет, пользователь!')
 
+def name_constellation(update, context):
+    user_planet = update.message.text.split()
+    if len(user_planet) > 2:
+        update.message.reply_text('Нужно передать только одну планету')
+    else:
+        get_planet = user_planet.pop().capitalize()
+        planets_list = [
+            Mars(), 
+            Jupiter(), 
+            Saturn(), 
+            Mercury(), 
+            Uranus(), 
+            Venus(), 
+            Neptune()
+        ]
+        for planet in planets_list:
+            if get_planet == planet.name:
+                planet.compute()
+                get_constellation = constellation(planet)
+                update.message.reply_text(f'Планета сейчас находится в созвездии {get_constellation}')
+          
 def talk_to_me(update, context):
     text = update.message.text
     print(text)
@@ -22,6 +45,7 @@ def main():
 
     dp = mybot.dispatcher
     dp.add_handler(CommandHandler('start', greet_user))
+    dp.add_handler(CommandHandler('planet', name_constellation))
     dp.add_handler(MessageHandler(Filters.text, talk_to_me))
 
     logging.info('Бот стартовал')
